83fca12b0d780ed4b2bc8d6e84871254d4fb3571
Karthik-Sunil-K <karthiksunil038@gmail.com>
2020-12-09 12:58:32 +0530

    cousera cert design

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index',);
 });
-
+router.post('/submit',function(req,res){
+  console.log(req.body)
+  res.render('certificate',{ fname:req.body.first_name,lname:req.body.last_name,course:req.body.course});
+  console.log(req.body.first_name)
+})
 module.exports = router;
